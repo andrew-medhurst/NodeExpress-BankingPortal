6a6d7056b4a09f6c0051e139f1b990cfc6cd7b95
Andrew Medhurst <amedhurst76@gmail.com>
2021-06-25 11:17:22 -0400

    completed module 6 tasks

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const { accounts, users, writeJSON } = require('./data');
+const accountRoutes = require('./routes/accounts');
+const servicesRoutes = require('./routes/services');
 
 const app = express();
 const dirPath = __dirname; 
@@ -10,6 +12,8 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(dirPath, '/public')));
 app.use(express.urlencoded({ extended: true }));
+app.use('/account', accountRoutes);
+app.use('/services', servicesRoutes);
 
 app.get('/', (req, res) => {
   res.render('index', { 
@@ -18,58 +22,9 @@ app.get('/', (req, res) => {
   });
 });
 
-app.get('/savings', (req, res) => {
-  res.render('account', {
-    account: accounts.savings
-  });
-});
-
-app.get('/credit', (req, res) => {
-  res.render('account', {
-    account: accounts.credit
-  });
-});
-
-app.get('/checking', (req, res) => {
-  res.render('account', {
-    account: accounts.checking
-  });
-});
-
 app.get('/profile', (req, res) => {
   res.render('profile', {
      user: users[0]
-  });
-});
-
-app.get('/transfer', (req, res) => {
-  res.render('transfer');
-});
-app.post('/transfer', (req, res) => {
-  const { from, to, amount } = req.body;
-  
-  accounts[from].balance -= amount;
-  accounts[to].balance += parseInt(amount);
-  
-  writeJSON();
-
-  res.render('transfer', { message: 'Transfer Completed' });
-});
-
-app.get('/payment', (req, res) => {
-  res.render('payment', {
-    account: accounts.credit
-  });
-});
-app.post('/payment', (req, res) => {
-  accounts.credit.balance -= req.body.amount;
-  accounts.credit.available += parseInt(req.body.amount);
-
-  writeJSON();
-  
-    res.render('payment', { 
-    message: 'Payment Successful',
-    account: accounts.credit
   });
 });
 
