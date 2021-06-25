0dcaaa695403151ba9765e969ad3661d19a8c929
Andrew Medhurst <amedhurst76@gmail.com>
2021-06-25 10:02:21 -0400

    tasks for module 4 completed

--- src/app.js
+++ src/app.js
@@ -8,12 +8,13 @@ app.set('views', path.join(dirPath, '/views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(dirPath, '/public')));
+app.use(express.urlencoded({ extended: true }));
 
 const accountData = fs.readFileSync(
-  path.join(dirPath, '/json/accounts.json'), {encoding: 'UTF-8'});
+  path.join(dirPath, '/json/accounts.json'), {encoding: 'UTF8'});
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(dirPath, '/json/users.json'), { encoding: 'UTF-8'});
+const userData = fs.readFileSync(path.join(dirPath, '/json/users.json'), { encoding: 'UTF8'});
 const users = JSON.parse(userData);
 
 app.get('/', (req, res) => {
@@ -47,6 +48,41 @@ app.get('/profile', (req, res) => {
   });
 });
 
+app.get('/transfer', (req, res) => {
+  res.render('transfer');
+});
+app.post('/transfer', (req, res) => {
+  const { from, to, amount } = req.body;
+  
+  accounts[from].balance -= amount;
+  accounts[to].balance += parseInt(amount);
+  
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(dirPath, '/json/accounts.json'), 
+    accountsJSON, { encoding: 'UTF8' });
+
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) => {
+  res.render('payment', {
+    account: accounts.credit
+  });
+});
+app.post('/payment', (req, res) => {
+  accounts.credit.balance -= req.body.amount;
+  accounts.credit.available += parseInt(req.body.amount);
+
+  const accountsJSON = JSON.stringify(accounts);
+  fs.writeFileSync(path.join(dirPath, '/json/accounts.json'), 
+    accountsJSON, { encoding: 'UTF8' });
+  
+    res.render('payment', { 
+    message: 'Payment Successful',
+    account: accounts.credit
+  });
+});
+
 app.listen(3000, () => {
   console.log('PS Project Running on port 3000!');
-})
+});
